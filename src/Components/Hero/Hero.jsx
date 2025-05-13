382bc6ff80772cd63d1e010bc1592ae91c99ab26
dassajib <sajibdas.sd@gmail.com>
2025-05-13 12:59:19 +0600

    hero sec's search field's responsive issue solved

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="max-w-[1440px] mx-auto">
       <div className="flex flex-col md:flex-row justify-around items-center gap-10 pt-24 pb-6">
-        <div className="flex flex-col gap-12">
+        <div className="flex flex-col gap-12 px-4 md:px-0">
           <div className="relative z-[1]">
             <div className="absolute w-16 h-16 right-[100px] -top-4 rounded-full bg-orange-400 z-[-1]" />
             <motion.h1
@@ -22,13 +22,28 @@ const Hero = () => {
             <span className="text-gray-400">Forget all difficulties in finding a residence for you</span>
           </div>
 
-          <div className="bg-white rounded-md border-[3px] border-[rgba(120,120,120,0.3)] flex justify-between items-center py-2 px-4">
+          {/* <div className="bg-white rounded-md border-[3px] border-[rgba(120,120,120,0.3)] flex justify-between items-center py-2 px-4">
             <HiLocationMarker color="#3D62F8" size={25} />
             <input className="border-none outline-none" type="text" />
             <button className="bg-[#3D62F8] text-white px-6 py-2 rounded-md text-[14px] transition duration-300 ease-in hover:scale-110">Search</button>
+          </div> */}
+
+          <div className="bg-white rounded-md border-[3px] border-[rgba(120,120,120,0.3)] flex flex-col sm:flex-row gap-3 sm:gap-0 sm:justify-between sm:items-center py-2 px-4">
+            <div className="flex items-center gap-2 w-full">
+              <HiLocationMarker color="#3D62F8" size={25} />
+              <input
+                className="border-none outline-none w-full"
+                type="text"
+                placeholder="Enter location"
+              />
+            </div>
+            <button className="bg-[#3D62F8] text-white px-6 py-2 rounded-md text-[14px] transition duration-300 ease-in hover:scale-110 w-full sm:w-auto">
+              Search
+            </button>
           </div>
 
-          <div className="flex justify-start items-center gap-4 text-white">
+
+          <div className="flex justify-start md:items-center gap-4 text-white">
             <div className="flex flex-col justify-center items-center">
               <span className="text-2xl md:text-4xl">
                 <CountUp start={8800} end={9000} duration={4} />
